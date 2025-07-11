d6d8355353d8e318186ff44e3d78ec524d63e709
fdhhhdjd <nguyentientai10@gmail.com>
2025-07-11 09:17:55 +0700

    feat: add more products to test standard

--- pos-barcode-backend/products.js
+++ pos-barcode-backend/products.js
@@ -3,5 +3,20 @@ module.exports = [
     { barcode: "1111111111", name: "Pepsi", price: 12000 },
     { barcode: "2222222222", name: "Sting", price: 9000 },
     { barcode: "3333333333", name: "Number One", price: 8000 },
-    { barcode: "4444444444", name: "Dr Thanh", price: 11000 }
+    { barcode: "4444444444", name: "Dr Thanh", price: 11000 },
+    { barcode: "5555555555", name: "Aquafina Water", price: 7000 },
+    { barcode: "6666666666", name: "Oishi Snack", price: 5000 },
+    { barcode: "7777777777", name: "Vinamilk Milk", price: 15000 },
+    { barcode: "8888888888", name: "Twister Orange", price: 13000 },
+    { barcode: "9999999999", name: "Revive Isotonic", price: 12000 },
+    { barcode: "8901234567", name: "Red Bull", price: 11000 },
+    { barcode: "8901234568", name: "Warrior Energy", price: 9000 },
+    { barcode: "8901234569", name: "Snack Poca", price: 7000 },
+    { barcode: "8901234570", name: "Yogurt TH True", price: 14000 },
+    { barcode: "8901234571", name: "Nutri Boost", price: 13000 },
+    { barcode: "8901234572", name: "Milo Box", price: 11000 },
+    { barcode: "8901234573", name: "TH True Water", price: 8000 },
+    { barcode: "8901234574", name: "C2 Green Tea", price: 9000 },
+    { barcode: "8901234575", name: "7Up", price: 10000 },
+    { barcode: "8901234576", name: "Sprite", price: 10500 }
 ];
